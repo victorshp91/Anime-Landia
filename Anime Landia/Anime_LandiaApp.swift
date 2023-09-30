62d4fbec489e35154c44755d50d642d2ca39beff
Victor Saint Hilaire <victorshp30@gmail.com>
2023-09-30 13:00:06 -0400

    Initial Commit

---
+++ Anime Landia/Anime_LandiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_LandiaApp.swift
+//  Anime Landia
+//
+//  Created by Victor Saint Hilaire on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Anime_LandiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anime Landia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anime Landia
+//
+//  Created by Victor Saint Hilaire on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
